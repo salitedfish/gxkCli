ffb129774507cf33a0033511a017a84766d1eedf
codergxk <1525185228@qq.com>
2023-04-26 14:54:20 +0800

    feat: init

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  printWidth: 300,
+  tabWidth: 2,
+  singleQuote: false,
+  semi: true,
+};
---
+++ rollup.config.ts
@@ -0,0 +1,90 @@
+import path from "path";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import rollupJSON from "@rollup/plugin-json";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import { eslint } from "rollup-plugin-eslint";
+import babel from "@rollup/plugin-babel";
+import genPackageJson from "rollup-plugin-generate-package-json";
+import copy from "rollup-plugin-copy";
+import packageJSON from "./package.json";
+// import { terser } from "rollup-plugin-terser";
+/**rollup-plugin test */
+
+const getPath = (_path: string) => path.resolve(__dirname, _path);
+
+/**解构package.json内容 */
+const { config, name, version, dependencies, description, author } = packageJSON;
+const { dest } = config;
+
+/**ts解析插件 */
+const tsPlugin = typescript({
+  tsconfig: "tsconfig.json",
+});
+
+/**eslint插件 */
+const esPlugin = eslint({
+  typescript: require("ttypescript"),
+  throwOnError: true,
+  include: ["src/**/*.ts", "type/**/*.d.ts"],
+  exclude: ["node_modules/**"],
+});
+
+/**babel插件 */
+const babelPlugin = babel({
+  exclude: "node_modules/**",
+  babelHelpers: "bundled",
+  extensions: [".ts", ".js"],
+});
+
+/**生成package.json插件 */
+const packageJsonPlugin = genPackageJson({
+  outputFolder: dest,
+  baseContents: () => ({
+    name,
+    version,
+    bin: {
+      gxk: "./index.cjs",
+      tiga: "./index.cjs",
+    },
+    author,
+    license: "MIT",
+    main: "index.cjs",
+    module: "index.mjs",
+    typings: "src/index.d.ts",
+    dependencies,
+    description,
+    sideEffects: false,
+    keywords: ["typescript", "library"],
+  }),
+});
+
+/**复制文件夹到指定文件夹 */
+const rollupCopy = copy({
+  targets: [{ src: "./readme.md", dest }],
+});
+
+export default () => {
+  return {
+    /**打包入口 */
+    input: getPath("./src/index.ts"),
+    /**排除外部引入的包 */
+    external: Object.keys(dependencies),
+    plugins: [
+      nodeResolve({}),
+      esPlugin,
+      rollupJSON(),
+      tsPlugin,
+      babelPlugin,
+      commonjs(),
+      // terser(),
+      packageJsonPlugin,
+      rollupCopy,
+    ],
+    /**同时支持ESModule和commonjs导出 */
+    output: [
+      { file: `${dest}/index.mjs`, format: "esm", name },
+      { file: `${dest}/index.cjs`, format: "cjs", name },
+    ],
+  };
+};
